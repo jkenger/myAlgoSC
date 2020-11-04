3adca036718efbe6ba18e8593edab34e98ed5a46
Ken <73566770+jkenger@users.noreply.github.com>
2020-11-04 21:05:18 +0800

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,65 @@
+﻿using CashierApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ItemNamespace
+{
+    public abstract class Item 
+    {
+        protected string item_name;
+        protected int item_quantity;
+        private double item_price, total_price;
+        public abstract double getTotalPrice();
+        public abstract void setPayment(double amount);
+
+        public Item(){}
+        //Initiate the item
+        public Item(string name, double price, int quantity)
+        {
+            this.item_name = name;
+            this.item_price = price;
+            this.item_quantity = quantity;
+            // Console.WriteLine(item_name + " " + item_price + " " + item_quantity);
+        }
+
+        // Compute the user's payment
+    }
+
+    class DiscountedItem : Item
+    {
+        private double item_discount;
+        private double discounted_price;
+
+        private double payment_amount;
+        private double change;
+
+        public DiscountedItem(string name, double price, int quantity, double discount) : base(name, price, quantity)
+        {
+            this.item_discount = discount * 0.01;
+            this.discounted_price = (price - (this.item_discount * price)) * quantity;
+        }
+
+        public override double getTotalPrice()
+        {
+            return discounted_price;
+        }
+
+        public override void setPayment(double amount)
+        {
+            this.payment_amount = amount;
+            this.change = payment_amount - discounted_price;
+        }
+
+        public double getChange()
+        {
+            return this.change;
+        }
+    }
+}
+
+
+
---
+++ frmPurchaseDiscountedItem.cs
@@ -0,0 +1,63 @@
+﻿using ItemNamespace;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashierApplication
+{
+    
+    public partial class frmPurchaseDiscountedItem : Form
+    {
+        DiscountedItem ditm;
+        public frmPurchaseDiscountedItem()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPurchaseDiscountedItem_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tbItem_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            string item = tbItem.Text;
+            double discount = Convert.ToDouble(tbDiscount.Text);
+            double price = Convert.ToDouble(tbPrice.Text);
+            int quantity = Convert.ToInt32(tbQuantity.Text);
+            ditm = new DiscountedItem(item, price, quantity, discount);
+
+            lblTotalAm.Text = "P" + ditm.getTotalPrice().ToString();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            double totalAmount = ditm.getTotalPrice();
+            double payment = Convert.ToDouble(tbPaymentRec.Text);
+            if (payment < totalAmount)
+            {
+                MessageBox.Show("Invalid amount", "Not enought payment", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                ditm.setPayment(payment);
+                lblChange.Text = "P" + ditm.getChange();
+            }
+        }
+    }
+}
+
+
+
